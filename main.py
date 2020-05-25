8f8dfc0a8330ab687f109f044d74f420e3050076
NidhishKumarReddy <nidhishkumarreddy@gmail.com>
2020-05-25 21:34:36 +0530

    created new file

---
+++ main.py
@@ -0,0 +1,22 @@
+import pygame
+
+# Initialize the pygame
+pygame.init()
+
+# Create Screen
+screen = pygame.display.set_mode((800,600))
+
+# Title and Icon
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load('icon.png')
+pygame.display.set_icon(icon)
+
+# Game Running function
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    # RGB
+    screen.fill((255, 0, 0))
+    pygame.display.update()
