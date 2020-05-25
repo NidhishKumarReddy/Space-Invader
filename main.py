637dc4e613459249706ef0b8ea095ad3cb9e725d
NidhishKumarReddy <nidhishkumarreddy@gmail.com>
2020-05-25 22:27:14 +0530

    updated main.py

--- main.py
+++ main.py
@@ -11,12 +11,37 @@ pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load('icon.png')
 pygame.display.set_icon(icon)
 
+# Player
+playerImg = pygame.image.load('spaceship.png')
+playerX = 370
+playerY = 480
+playerX_change = 0
+
+def player(x, y):
+    screen.blit(playerImg, (x, y))
+
 # Game Running function
 running = True
 while running:
+    
+    # RGB
+    screen.fill((0, 0, 0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    # RGB
-    screen.fill((0, 0, 0))
+
+        #if keystroke is pressed check whether its rigth or left
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -0.3
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 0.3
+        
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
+    
+    playerX += playerX_change
+    player(playerX, playerY)
     pygame.display.update()
